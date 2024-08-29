ec8f8d3ffebddbd7051e3733df044f4557a11c1b
Felipe Cardoso <lymeicontato@gmail.com>
2024-08-28 21:49:17 -0300

    feat: create navbar with drawer to mobile

---
+++ src/app/_components/MobileSidebar.tsx
@@ -0,0 +1,82 @@
+'use client';
+
+import { ICON_SIZE, NAVBAR_SECTIONS } from '@/lib/constants';
+import { cn } from '@/lib/utils';
+import Link from 'next/link';
+import { useEffect, useRef, useState } from 'react';
+import { FaMoneyBill, FaPlus } from 'react-icons/fa6';
+import { IoIosLock } from 'react-icons/io';
+
+export default function MobileSidebar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const drawerRef = useRef<HTMLDivElement>(null);
+  const isFirstTouch = useRef(true);
+
+  useEffect(() => {
+    if (!isOpen) {
+      isFirstTouch.current = true;
+      return;
+    }
+
+    function handleTouch(event: MouseEvent) {
+      if (!drawerRef.current?.contains(event.target as Node)) {
+        if (isFirstTouch.current) isFirstTouch.current = false;
+        else setIsOpen(false);
+      }
+    }
+
+    window.addEventListener('click', handleTouch);
+
+    return () => window.removeEventListener('click', handleTouch);
+  }, [isOpen]);
+
+  return (
+    <nav className='grid grid-cols-3 gap-4 p-2 text-sm sm:hidden'>
+      <Link href='/' className='flex flex-col items-center gap-1 p-2'>
+        <FaMoneyBill size={ICON_SIZE.SMALL} />
+        <p>Transações</p>
+      </Link>
+      <Link href='/' className='flex flex-col items-center gap-1 p-2'>
+        <IoIosLock size={ICON_SIZE.SMALL} />
+        <p>Home</p>
+      </Link>
+      <button
+        onClick={() => setIsOpen(true)}
+        className='flex flex-col items-center gap-1 p-2'
+      >
+        <FaPlus size={ICON_SIZE.SMALL} />
+        <p>Mais</p>
+      </button>
+      <div
+        className={cn(
+          'absolute bottom-0 left-0 right-0 top-1/2 z-40 rounded bg-neutral-950 transition-transform',
+          { 'translate-y-full': !isOpen, 'translate-y-0': isOpen },
+        )}
+        ref={drawerRef}
+      >
+        <div className='pointer-events-none absolute left-0 right-0 top-0 h-12 bg-gradient-to-b from-black' />
+        <div className='h-full w-full space-y-12 overflow-auto p-6'>
+          {NAVBAR_SECTIONS.map((section, sectionIndex) => (
+            <div key={`mobile-section-${section.name}`} className='space-y-6'>
+              <p className='text-xl font-bold'>{section.name}</p>
+              <div className='grid grid-cols-3 gap-4'>
+                {section.navbarItems.map((item, itemIndex) => (
+                  <Link
+                    href={item.href ?? ''}
+                    key={`mobile-section-${section.name}-item-${item.name}`}
+                    className='flex flex-col items-center gap-2 text-center'
+                  >
+                    <item.icon size={ICON_SIZE.LARGE} />
+                    <p>{item.name}</p>
+                  </Link>
+                ))}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      {isOpen && <div className='absolute inset-0 backdrop-blur-[1px]' />}
+    </nav>
+  );
+}
