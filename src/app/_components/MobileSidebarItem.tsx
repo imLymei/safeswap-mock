d7e839463b2fb1fd3659982d15047a1744612900
Felipe Cardoso <lymeicontato@gmail.com>
2024-08-29 21:42:53 -0300

    style: add border to mobile buttons

--- src/app/_components/MobileSidebarItem.tsx
+++ src/app/_components/MobileSidebarItem.tsx
@@ -9,7 +9,7 @@ export default function MobileSidebarItem({
   return (
     <Link
       href={href}
-      className='flex aspect-square flex-col items-center justify-between gap-2 rounded p-4 text-center active:bg-blue-500/20'
+      className='flex aspect-square flex-col items-center justify-between gap-2 rounded border border-blue-500/20 p-4 text-center active:bg-blue-500/20'
     >
       <Icon size={ICON_SIZE.MOBILE} />
       <p className='line-clamp-2'>{name}</p>
